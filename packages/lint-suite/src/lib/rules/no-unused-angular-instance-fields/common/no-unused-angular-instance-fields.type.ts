2049b2ffc22a5edbfdad651e96e9c564dee67441
Artiom Tofan <artiomtofancv@gmail.com>
2026-08-30 00:47:08 +0200

    feat(types): ✨ add types for Angular class and imports

---
+++ packages/lint-suite/src/lib/rules/no-unused-angular-instance-fields/common/no-unused-angular-instance-fields.type.ts
@@ -0,0 +1,16 @@
+import type { TSESTree } from '@typescript-eslint/utils';
+
+export type AngularClassNode =
+  | TSESTree.ClassDeclaration
+  | TSESTree.ClassExpression;
+
+export type AngularImport = string | null;
+
+export type AngularImports = Map<string, AngularImport>;
+
+export type ClassEntry = {
+  readonly node: AngularClassNode;
+  readonly reads: Set<string>;
+};
+
+export type DynamicClasses = Set<ClassEntry>;
