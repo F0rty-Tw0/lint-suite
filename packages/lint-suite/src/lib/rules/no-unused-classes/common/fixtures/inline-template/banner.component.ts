1a49767e498264e813dc231cb958b40fdff5aad3
Artiom Tofan <artiomtofancv@gmail.com>
2026-09-06 00:27:12 +0200

    feat(fixtures): ✨ add new components and styles for no-unused-classes

---
+++ packages/lint-suite/src/lib/rules/no-unused-classes/common/fixtures/inline-template/banner.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-banner',
+  template: '<div class="a"></div>',
+  styleUrl: './banner.component.scss'
+})
+export class BannerComponent {}
